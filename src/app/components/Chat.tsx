312e2aefbdee532521c1f5718faa6fe8c44cf045
Amar Prasad <amarprasad77354@gmail.com>
2023-07-08 16:48:36 +0530

    accordion

--- src/app/components/Chat.tsx
+++ src/app/components/Chat.tsx
@@ -1,7 +1,15 @@
+import { Accordion } from "@/components/ui/accordion";
+import { AccordionItem } from "@radix-ui/react-accordion";
 import { FC } from "react";
 
 const Chat: FC = () => {
-    return <div>Chat</div>
+    return (
+        <Accordion type="single" collapsible className="relative bg-white z-40 ">
+            <AccordionItem value="item-1">
+                <div className="fixed right-8 w-80 bottom-8 bg-white border border-gray-200 rounded-md overflow-hidden"></div>
+            </AccordionItem> 
+        </Accordion>
+    );
 }
 
 export default Chat
