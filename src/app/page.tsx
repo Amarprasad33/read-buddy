4851d811b1054a418f802d9fafea31452b9378ac
Amar Prasad <amarprasad77354@gmail.com>
2023-07-08 16:24:49 +0530

    ini-comp

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,7 +2,7 @@ import Image from 'next/image'
 
 export default function Home() {
   return (
-    <main className="">
+    <main className="absolute inset-0 flex justify-center items-center">
       Website Content
     </main>
   )
