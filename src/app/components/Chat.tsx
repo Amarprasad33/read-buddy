44f442f815f69d402ef037a8a3b47b3c23865cbe
Amar Prasad <amarprasad77354@gmail.com>
2023-07-08 17:14:50 +0530

    chatbot header

--- src/app/components/Chat.tsx
+++ src/app/components/Chat.tsx
@@ -1,12 +1,19 @@
-import { Accordion } from "@/components/ui/accordion";
-import { AccordionItem } from "@radix-ui/react-accordion";
+import { Accordion } from "@/app/components/ui/accordion";
+import { AccordionItem, AccordionTrigger } from "@/app/components/ui/accordion";
 import { FC } from "react";
+import ChatHeader from "./ChatHeader";
 
 const Chat: FC = () => {
     return (
         <Accordion type="single" collapsible className="relative bg-white z-40 ">
             <AccordionItem value="item-1">
-                <div className="fixed right-8 w-80 bottom-8 bg-white border border-gray-200 rounded-md overflow-hidden"></div>
+                <div className="fixed right-8 w-80 bottom-8 bg-white border border-gray-200 rounded-md overflow-hidden">
+                    <div className="w-full h-full flex flex-col">
+                        <AccordionTrigger className="px-6 border-b border-zinc-300">
+                            <ChatHeader />
+                        </AccordionTrigger>
+                    </div>
+                </div>
             </AccordionItem> 
         </Accordion>
     );
