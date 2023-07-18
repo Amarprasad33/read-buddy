099062e431930623267d3d043eab2872fc515571
Amar Prasad <amarprasad77354@gmail.com>
2023-07-18 21:34:27 +0530

    stream-fetch

--- src/lib/openai-stream.ts
+++ src/lib/openai-stream.ts
@@ -1,4 +1,4 @@
-import { ParsedEvent, ReconnectInterval } from "eventsource-parser"
+import { ParsedEvent, ReconnectInterval, createParser } from "eventsource-parser"
 
 export type ChatGPTAgent = "user" | "system"
 
@@ -38,9 +38,38 @@ export async function OpenAIStream(payload: OpenAIStreamPayload){
     const stream = new ReadableStream({
         async start(controller) {
             function onParse(event: ParsedEvent | ReconnectInterval){
-                
+                if(event.type === 'event'){
+                    const data = event.data;
+                    if(data === '[DONE]'){
+                        controller.close();
+                        return;
+                    }
+
+                    try {
+                        const json = JSON.parse(data);
+                        const text = json.choices[0].delta?.content || '';
+
+                        if(counter < 2 && (text.match(/n/) || []).length){
+                            return
+                        }
+
+                        const  queue = encoder.encode(text);
+                        controller.enqueue(queue);
+
+                        counter++;
+
+                    } catch (error) {
+                        controller.error(error)
+                    }
+                }
             }
-        }
+
+            const parser = createParser(onParse);
+
+            for await (const chunk of res.body as any){
+                parser.feed(decoder.decode(chunk))
+            }
+        },
     })
 }
 
